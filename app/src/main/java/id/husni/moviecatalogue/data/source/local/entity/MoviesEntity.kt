322015b7ce4618fa0c31f86615933182b0365757
whoishusni <mohhusnimubaraq@gmail.com>
2020-12-20 15:30:06 +0800

    minor change checkpoint

--- app/src/main/java/id/husni/moviecatalogue/data/source/local/entity/MoviesEntity.kt
+++ app/src/main/java/id/husni/moviecatalogue/data/source/local/entity/MoviesEntity.kt
@@ -13,28 +13,22 @@ import kotlinx.android.parcel.Parcelize
 data class MoviesEntity(
     @PrimaryKey
     @NonNull
-    @ColumnInfo(name = "overview")
-    @field:SerializedName("overview")
-    val overview: String,
-
-    @ColumnInfo(name = "release_date")
-    @field:SerializedName("release_date")
-    val releaseDate: String,
-
-    @ColumnInfo(name = "vote_average")
-    @field:SerializedName("vote_average")
-    val voteAverage: Double,
-
     @ColumnInfo(name = "id")
-    @field:SerializedName("id")
     val id: Int,
 
     @ColumnInfo(name = "title")
-    @field:SerializedName("title")
     val title: String,
 
+    @ColumnInfo(name = "release_date")
+    val releaseDate: String,
+
+    @ColumnInfo(name = "vote_average")
+    val voteAverage: Double,
+
+    @ColumnInfo(name = "overview")
+    val overview: String,
+
     @ColumnInfo(name = "poster_path")
-    @field:SerializedName("poster_path")
     val posterPath: String,
 
     @ColumnInfo(name = "bookmarked")
